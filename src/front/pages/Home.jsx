897271a39d06853f7c14e379b112b6e098b071ee
AzaleaMoon10 <carla.clemente.010@gmail.com>
2026-03-02 09:48:44 +0000

    home-finished

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -4,31 +4,26 @@ export const Home = () => {
 	return (
 		<div className="text-center">
 			<div className="w-100 mx-auto p-5 jumbotron-image">
-				<div className="container-fluid">
-					<div className="container ">
+				<div className="container-fluid ">
+					<div className="container">
 						<h1 className="display-3 text-color-jumbo-h1">Welcome, gamer!</h1>
-						<p className="text-color-jumbo-p">This website will help you keep your backlog of games in order. You can also rate them and specify their current status. You are welcomed to share your profile with friends so they can see it!</p>
 					</div>
 				</div>
 			</div>
-			<div className="container">
-				<div className="row">
-					<div className="col-md-4">
-						<h2>Heading</h2>
-						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-						<p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-					</div>
-					<div className="col-md-4">
-						<h2>Heading</h2>
-						<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-						<p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-					</div>
-					<div className="col-md-4">
-						<h2>Heading</h2>
-						<p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-						<p><a className="btn btn-secondary" href="#" role="button">View details »</a></p>
-					</div>
-				</div>
+			<div className="home-container p-1 mx-auto mt-5 rounded game-card">
+				<h4 className="text-color-jumbo-p ">This website is designed to help you keep your backlog of games in order.</h4>
+			</div>
+			<div className="container img-cont-home mt-5">
+				<div className="mx-auto p-5 jumbotron-image-2"></div>
+			</div>
+			<div className="home-container p-1 mx-auto mt-5 game-card rounded">
+				<h4 className="text-color-jumbo-p">Track the status of your games!</h4>
+			</div>
+			<div className="container img-cont-home mt-5">
+				<div className="mx-auto p-5 jumbotron-image-3"></div>
+			</div>
+			<div className="home-container p-1 mx-auto mt-5 game-card rounded">
+				<h4 className="text-color-jumbo-p">Share your profile with your friends!</h4>
 			</div>
 		</div>
 	);
